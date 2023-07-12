96e562104294e14a617f5017802da65e87464ca8
AndrewVorotyntsev <andrey12092000@gmail.com>
2023-07-12 12:15:02 +0300

    Цвет фона

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,7 @@ class _MyHomePageState extends State<MyHomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: Text(widget.title),
